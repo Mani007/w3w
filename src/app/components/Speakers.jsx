66ac74e0f3e4a6a637a001ba9688a25a13b18a4b
Mohnish <mohnish.a007@gmail.com>
2024-03-01 20:01:27 +0000

    image width issue resolved

--- src/app/components/Speakers.jsx
+++ src/app/components/Speakers.jsx
@@ -7,31 +7,31 @@ function Speakers() {
   return (
     <>
     <div className='container mx-auto py-36 px-8'>
-        <div className='grid lg:grid-col-3'>
+        <div className=' grid lg:grid-cols-4 gap-4'>
             {speakers.map((speaker,index) =>
             (
             
-             <div key={speaker.id}>
+             <div className='shadow-sm rounded-sm text-center' key={speaker.id}>
                 <Image
                 src={speaker.imgSrc}
-                
+                className='rounded-t-lg'  
+                style={{ "width": "100%"}}
                 alt={speaker.name}
                 height={150}
-                width={150}       
+                width={250}   
+                    
                       />
-
+                <p>{speaker.name}</p> 
+                <p>{speaker.jobtitle}</p>
+                <p>{speaker.company}</p> 
+                 <p>{speaker.social}</p>
              </div>   
             ) 
             )
             
             }
         </div>
-        <Image
-                src={dog}
-                alt="abc"
-                height={150}
-                width={150}       
-                         />
+        
 
     </div>
     </>
