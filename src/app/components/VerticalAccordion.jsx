e9168d06396f4d06755d25c197fceb7630b84f11
Mohnish <mohnish.a007@gmail.com>
2024-03-01 12:32:07 +0000

    List of item in vertical timeline working successfully

--- src/app/components/VerticalAccordion.jsx
+++ src/app/components/VerticalAccordion.jsx
@@ -3,12 +3,13 @@ import { FiBarChart, FiBell, FiDollarSign, FiPlay } from "react-icons/fi";
 import { AnimatePresence, motion } from "framer-motion";
 import { useWindowSize } from "../utils/useWindowSize";
 import { useState } from "react";
+import { Ultra } from "next/font/google";
 
 const VerticalAccordion = () => {
   const [open, setOpen] = useState(items[0].id);
 
   return (
-    <section className="p-4 bg-indigo-600">
+    <section className="p-4 bg-[#98B4AC]">
       <div className="flex flex-col lg:flex-row h-fit lg:h-[450px] w-full max-w-6xl mx-auto shadow overflow-hidden">
         {items.map((item) => {
           return (
@@ -17,9 +18,18 @@ const VerticalAccordion = () => {
               open={open}
               setOpen={setOpen}
               id={item.id}
-              Icon={item.Icon}
+              plan={item.plan.map(
+                (i,index)=>{
+                  return (
+                      
+                      <li key={index}>{i}</li>
+                   );
+                }
+                
+              )}
+              //Icon={item.Icon}
               title={item.title}
-              imgSrc={item.imgSrc}
+              //imgSrc={item.imgSrc}
               description={item.description}
             />
           );
@@ -29,14 +39,14 @@ const VerticalAccordion = () => {
   );
 };
 
-const Panel = ({ open, setOpen, id, Icon, title, imgSrc, description }) => {
+const Panel = ({ open, setOpen, id, plan, Icon, title, imgSrc, description }) => {
   const { width } = useWindowSize();
   const isOpen = open === id;
 
   return (
     <>
       <button
-        className="bg-white hover:bg-slate-50 transition-colors p-3 border-r-[1px] border-b-[1px] border-slate-200 flex flex-row-reverse lg:flex-col justify-end items-center gap-4 relative group"
+        className="bg-[#C9B46D] hover:bg-[#5E95A2] transition-colors p-3 border-r-[1px] border-b-[1px] border-slate-200 flex flex-row-reverse lg:flex-col justify-end items-center gap-4 relative group"
         onClick={() => setOpen(id)}
       >
         <span
@@ -48,9 +58,9 @@ const Panel = ({ open, setOpen, id, Icon, title, imgSrc, description }) => {
           {title}
         </span>
         <span className="block lg:hidden text-xl font-light">{title}</span>
-        <div className="w-6 lg:w-full aspect-square bg-indigo-600 text-white grid place-items-center">
+        {/* <div className="w-6 lg:w-full aspect-square bg-indigo-600 text-white grid place-items-center">
           <Icon />
-        </div>
+        </div> */}
         <span className="w-4 h-4 bg-white group-hover:bg-slate-50 transition-colors border-r-[1px] border-b-[1px] lg:border-b-0 lg:border-t-[1px] border-slate-200 rotate-45 absolute bottom-0 lg:bottom-[50%] right-[50%] lg:right-0 translate-y-[50%] translate-x-[50%] z-20" />
       </button>
 
@@ -63,20 +73,24 @@ const Panel = ({ open, setOpen, id, Icon, title, imgSrc, description }) => {
             animate="open"
             exit="closed"
             style={{
-              backgroundImage: `url(${imgSrc})`,
+              //backgroundImage: `url(${imgSrc})`,
               backgroundPosition: "center",
               backgroundSize: "cover",
             }}
-            className="w-full h-full overflow-hidden relative bg-black flex items-end"
+            className="w-full h-full overflow-hidden relative bg-[#F2F5EC] flex items-center"
           >
             <motion.div
               variants={descriptionVariants}
               initial="closed"
               animate="open"
               exit="closed"
-              className="px-4 py-2 bg-black/40 backdrop-blur-sm text-white"
-            >
-              <p>{description}</p>
+              className="px-4 py-2 bg-[#98B4AC] backdrop-blur-sm text-white"
+              >
+              {plan}
+
+              <div>
+              </div>
+            {/* <p>{description}</p> */}
             </motion.div>
           </motion.div>
         )}
@@ -123,38 +137,42 @@ const descriptionVariants = {
 const items = [
   {
     id: 1,
-    title: "Earn more",
-    Icon: FiDollarSign,
-    imgSrc:
-      "https://images.unsplash.com/photo-1553729459-efe14ef6055d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+    title: "Day 1 - 8 March 2024",
+    plan: [ "Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.", ],
+    //Icon: FiDollarSign,
+    // imgSrc:
+    //   "https://images.unsplash.com/photo-1553729459-efe14ef6055d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eius deserunt quia consectetur aliquid obcaecati voluptatibus quos distinctio natus! Tenetur.",
   },
   {
     id: 2,
-    title: "Play more",
-    Icon: FiPlay,
-    imgSrc:
-      "https://images.unsplash.com/photo-1541532713592-79a0317b6b77?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80",
+    title: "Day 2 - 9 March 2024",
+    plan: [ "Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.", ],
+    //Icon: FiPlay,
+    // imgSrc:
+    //   "https://images.unsplash.com/photo-1541532713592-79a0317b6b77?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eius deserunt quia consectetur aliquid obcaecati voluptatibus quos distinctio natus! Tenetur.",
   },
   {
     id: 3,
-    title: "Keep track",
-    Icon: FiBell,
-    imgSrc:
-      "https://images.unsplash.com/photo-1578450671530-5b6a7c9f32a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    title: "Day 3 - 10 March 2024",
+    plan: [ "Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.", ],
+    //Icon: FiBell,
+    // imgSrc:
+    //   "https://images.unsplash.com/photo-1578450671530-5b6a7c9f32a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eius deserunt quia consectetur aliquid obcaecati voluptatibus quos distinctio natus! Tenetur.",
   },
   {
     id: 4,
-    title: "Grow faster",
-    Icon: FiBarChart,
-    imgSrc:
-      "https://images.unsplash.com/photo-1543286386-713bdd548da4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
-    description:
+    title: "Online Schedule",
+    plan: [ "Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet.", ],
+    //Icon: FiBarChart,
+    // imgSrc:
+    //   "https://images.unsplash.com/photo-1543286386-713bdd548da4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eius deserunt quia consectetur aliquid obcaecati voluptatibus quos distinctio natus! Tenetur.",
   },
 ];
