3b43857a1cdb9cf3b33f8275b69954f9be875af1
Mohnish <mohnish.a007@gmail.com>
2024-03-02 15:34:50 +0000

    Prod ready:translate z updated to 200

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } p-6 black-gradient bg-[#F2F5EC] absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-200 rounded-xl`}
+            } p-6 black-gradient bg-[#F2F5EC] absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-[200] rounded-xl`}
           >
             <ul className='list-none flex justify-end items-end flex-1 flex-col gap-4'>
               {navLinks.map((nav) => (
