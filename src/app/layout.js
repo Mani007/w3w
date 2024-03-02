84b245dd97f48f3af44d7d47e00cca031dfa803b
Mohnish <mohnish.a007@gmail.com>
2024-03-02 15:58:35 +0000

    Prod ready:title updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Web3Women",
+  title: "Web3 Women Summit24",
   description: "Web3 Women summit 2024 is happening on 8-10 March 2024. ",
 };
 
