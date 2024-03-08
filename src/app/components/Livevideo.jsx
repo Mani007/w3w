5278a50bef8cf457658f166ef17d12f7a5434e36
Mohnish <mohnish.a007@gmail.com>
2024-03-08 02:27:37 +0000

    youtube removed

--- src/app/components/Livevideo.jsx
+++ src/app/components/Livevideo.jsx
@@ -7,7 +7,7 @@ function Livevideo() {
     <p className='py-8 text-white text-center text-4xl font-bold'>WIITNESS HISTORY IN THE MAKING - LIVE!!!</p>
       <div className='flex w-full h-full justify-center'>
 
-      <iframe width="560" height="315"  src="https://www.youtube.com/embed/live_stream?channel=UC0AtTJm4MegCmYIetnJeiVA" title="Web3 Women Summit - Live Streaming" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      {/* <iframe width="560" height="315"  src="https://www.youtube.com/embed/live_stream?channel=UC0AtTJm4MegCmYIetnJeiVA" title="Web3 Women Summit - Live Streaming" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe> */}
       
       </div>
     </div>
