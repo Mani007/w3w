123b57ed9100871e3583ee8e9feac5b52fe6cc07
Mohnish <mohnish.a007@gmail.com>
2024-03-08 17:39:31 +0000

    Spelling updated from live video

--- src/app/components/Livevideo.jsx
+++ src/app/components/Livevideo.jsx
@@ -4,7 +4,7 @@ function Livevideo() {
   return (
     <>
     <div className="p-4 w-full bg-gradient-to-r  from-[#FC8E9F] via-[#FC72B9] to-[#FCBB78]">
-    <p className='py-8 text-white text-center text-4xl font-bold'>WIITNESS HISTORY IN THE MAKING - LIVE!!!</p>
+    <p className='py-8 text-white text-center text-4xl font-bold'>WITNESS HISTORY IN THE MAKING - LIVE!!!</p>
       <div className='flex w-full h-full justify-center'>
 
       {/* <iframe width="560" height="315"  src="https://www.youtube.com/embed/live_stream?channel=UC0AtTJm4MegCmYIetnJeiVA" title="Web3 Women Summit - Live Streaming" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe> */}
