314a3c3e604da54ab788e529f2c448b4dc5132cb
Mohnish <mohnish.a007@gmail.com>
2024-03-11 09:51:22 +0000

    Aftermovive holder

--- src/app/components/Livevideo.jsx
+++ src/app/components/Livevideo.jsx
@@ -4,10 +4,11 @@ function Livevideo() {
   return (
     <>
     <div className="p-4 w-full bg-gradient-to-r  from-[#FC8E9F] via-[#FC72B9] to-[#FCBB78]">
-    <p className='py-8 text-white text-center text-4xl font-bold'>WITNESS HISTORY IN THE MAKING - LIVE!!!</p>
+    <p className='py-8 text-white text-center text-4xl font-bold'>THANK YOU ALL FOR JOINING US!!!</p>
+    <p className='py-8 text-white text-center text-4xl font-bold'>Event AFTER-MOVIE coming soon...</p>
       <div className='flex w-full h-full justify-center'>
 
-      <iframe width="560" height="315"  src="https://www.youtube.com/embed/live_stream?channel=UCGPlYf3m7EE_1OfvAR6cmLA" title="Web3 Women Summit - Live Streaming" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      {/* <iframe width="560" height="315"  src="https://www.youtube.com/embed/live_stream?channel=UCGPlYf3m7EE_1OfvAR6cmLA" title="Web3 Women Summit - Live Streaming" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe> */}
        {/* <iframe width="560" height="315"  src="https://streamyard.com/rnapek4hxu" title="Web3 Women Summit - Live Streaming" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
        */}
       </div>
